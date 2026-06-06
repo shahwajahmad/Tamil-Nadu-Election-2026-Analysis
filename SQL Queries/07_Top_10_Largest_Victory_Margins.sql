6271a20febab026ab144a36cb3eb50f6ed570ed9
Shahwaj Ahmad <158546201+shahwajahmad@users.noreply.github.com>
2026-06-06 17:15:36 +0530

    Add files via upload

---
+++ SQL Queries/07_Top_10_Largest_Victory_Margins.sql
@@ -0,0 +1,23 @@
+WITH ranked AS (
+    SELECT *,
+           ROW_NUMBER() OVER(
+               PARTITION BY ac_number
+               ORDER BY votes DESC
+           ) rn
+    FROM tn_2026_results
+)
+
+SELECT
+    w.constituency,
+    w.candidate AS winner,
+    r.candidate AS runner_up,
+    w.party AS winner_party,
+    r.party AS runner_up_party,
+    (w.votes - r.votes) AS margin
+FROM ranked w
+JOIN ranked r
+ON w.ac_number = r.ac_number
+WHERE w.rn = 1
+AND r.rn = 2
+ORDER BY margin DESC
+LIMIT 10;
